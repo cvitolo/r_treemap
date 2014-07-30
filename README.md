4953e97c4a94e13ba76d2a6c586fa0f2673a7517
Claudia Vitolo <claudia.vitolo@gmail.com>
2014-07-30 21:17:10 +0100

    First development version

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-r_treemap
+Generate tree maps from excel table (R package)
 =========
 
-Generate tree maps from excel table (R package)
+The package treeMap is a set of function that automatically generates tree maps for each sheet in a given excel file.
+
+# Test this package with the following commands:
+
+library(treeMap)
+
+writeMapTrees(system.file('table.xls', package = 'treeMap'))
---
+++ treeMap/R/makeList.R
@@ -0,0 +1,29 @@
+#' Function to make the list
+#' 
+#' @author Claudia Vitolo <claudia.vitolo@gmail.com>
+#' 
+#' @description Given a table in input a list is created to generate the tree nodes
+#' 
+#' @param x table containing 
+#' 
+#' @return list of tree nodes
+#' 
+#' @examples 
+#' # makelist(x)
+#' 
+
+makeList <- function(x){
+  
+  if(ncol(x)>2){
+    
+    listSplit<-split(x[-1],x[1],drop=T)
+    lapply(names(listSplit),
+           function(y){list(name=y,children=makeList(listSplit[[y]]))})
+    
+  }else{
+    
+    lapply(seq(nrow(x[1])),function(y){list(name=x[,1][y],size=x[,2][y])})
+    
+  }
+  
+}
